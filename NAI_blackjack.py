e7c01003ed9a86739679c81794a3d95188fa44c4
vechten <hk190696@gmail.com>
2021-10-22 23:50:05 +0200

    zmiany aby ai zaczynalo

--- NAI_blackjack.py
+++ NAI_blackjack.py
@@ -6,10 +6,11 @@ import random
 
 from easyAI import TwoPlayerGame, Human_Player, AI_Player, Negamax
 
+
 class GameBlackJack(TwoPlayerGame):
     '''
     A class to represent a game Blackjack with using AI
-    
+
     Attributes
     -----------------
     players : list
@@ -24,15 +25,16 @@ class GameBlackJack(TwoPlayerGame):
         player 1 has ended the game (lost or won)
     finish_player_2 : bool
         player 1 has ended the game (lost or won)
-        
-        
-    '''    
+
+
+    '''
+
     # THE PLANNING PHASE
 
     def __init__(self, players):
         self.players = players
-        self.cards_value_player = random.randint(1, 11) + random.randint(1, 11)  # start with 2 random cards
-        self.cards_value_player_2 = random.randint(1, 11) + random.randint(1, 11)  # start with 2 random cards
+        self.cards_value_player = random.randint(1, 11)   # start with 2 random cards
+        self.cards_value_player_2 = random.randint(1, 11)  # start with 2 random cards
         self.current_player = 1  # player 1 starts
         self.finish_player_1 = False
         self.finish_player_2 = False
@@ -42,14 +44,21 @@ class GameBlackJack(TwoPlayerGame):
     1 - hit card
     2 - stop taking card
     '''
+
     def possible_moves(self):
-        return [1, 2]
+        if self.current_player == 1 and self.finish_player_1:
+            return [2]
+        elif self.current_player == 2 and self.finish_player_2:
+            return [2]
+        else:
+            return [1, 2]
 
     '''
     make move
     1 - take card and add to your card value
     2 - stop taking card for the rest of a game
     '''
+
     def make_move(self, move):
         if self.current_player == 1 and not self.finish_player_1:
             if move == 1:
@@ -61,35 +70,49 @@ class GameBlackJack(TwoPlayerGame):
                 self.cards_value_player_2 += random.randint(1, 11)  # add radnom card.
             else:
                 self.finish_player_2 = True
+
     '''
     game is over when someone has more than 21 card value or both players stop taking cards
     '''
+
     def is_over(self):
         return (self.finish_player_1 and self.finish_player_2) or (
-                    self.cards_value_player > 21 or self.cards_value_player_2 > 21)
+                self.cards_value_player > 21 or self.cards_value_player_2 > 21)
+
     '''
     show communicate after before play
     '''
+
     def show(self):
         print(str(self.cards_value_player) + " cards value in your hand, and " + str(
             self.cards_value_player_2) + " value in ai hand. Press 1 to hit or 2 to stay")
+
     '''
-    help ai to make correct decision
+    help ai to make correct play
     '''
+
     def scoring(self):
         if self.current_player == 2:
-            if self.cards_value_player_2 > 21:
-                return -100
+            if (self.cards_value_player_2 > self.cards_value_player) and self.finish_player_1:
+                return 21
+            elif self.cards_value_player_2 > 21 or self.cards_value_player_2 < 11:
+                return -20
+            elif self.cards_value_player_2 < self.cards_value_player:
+                return -20
             else:
                 return self.cards_value_player_2
 
         if self.current_player == 1:
-            if self.cards_value_player > 21:
-                return -100
+            if self.cards_value_player > 21 or self.cards_value_player < 11:
+                return -20
+            elif (self.cards_value_player > self.cards_value_player_2) and self.finish_player_2:
+                return 20
+            elif self.cards_value_player < self.cards_value_player_2:
+                return -20
             else:
                 return self.cards_value_player
 
 
-ai = Negamax(4)
+ai = Negamax(6)
 game = GameBlackJack([Human_Player(), AI_Player(ai)])
 history = game.play()
